7c76de22ee637c688346e6125bf7ab64d17c753d
Shyam Kumar <149169925+HoneyKumar8@users.noreply.github.com>
2025-11-15 14:01:28 +0530

    Update and rename App.js.txt to App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Resume from './components/Resume';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Certifications from './components/Certifications';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Resume />
+      <Skills />
+      <Projects />
+      <Certifications />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
